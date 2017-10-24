fc789e2203a96ce25711f78486229dd2b6440194
Chris Johnson <jhnsn.c@gmail.com>
2017-10-23 22:54:51 -0500

    multiple build outputs

--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -1,37 +1,53 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const dirSource = path.join(__dirname, 'src');
-
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/knob-input.js',
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
+module.exports = function(env) {
+
+  const outputTarget = {
+    // path set below
     filename: 'knob-input.js',
     library: 'KnobInput',
     libraryExport: 'default',
-    libraryTarget: 'window',
-  },
-  module: {
-    loaders: [
-      // JS
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          comments: true,
-          compact: true,
-        },
-      }
+    // libraryTarget set below
+  };
+
+  if (!env || !env.target) {
+    return false;
+  } else if (env.target === 'window') {
+    outputTarget.path = path.join(__dirname, 'scripts');
+    outputTarget.libraryTarget = 'window';
+  } else if (env.target === 'umd') {
+    outputTarget.path = path.join(__dirname, 'umd');
+    outputTarget.libraryTarget = 'umd';
+  } else if (env.target === 'common') {
+    outputTarget.path = path.join(__dirname, 'common');
+    outputTarget.libraryTarget = 'commonjs2';
+  } else {
+    return false;
+  }
+
+  return {
+    entry: {
+      main: './src/knob-input.js',
+    },
+    output: outputTarget,
+    module: {
+      loaders: [
+        // JS
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            comments: true,
+            compact: true,
+          },
+        }
+      ]
+    },
+    plugins: [
+      new UglifyJSPlugin()
     ]
-  },
-  plugins: [
-    new UglifyJSPlugin()
-  ]
+  };
+
 };
