e3db0568111ab760b9f13d7690efe2111bb8b417
Chris Johnson <jhnsn.c@gmail.com>
2017-11-08 00:28:48 -0600

    exposing new dial component

--- src/fl-controls/index.js
+++ src/fl-controls/index.js
@@ -3,11 +3,13 @@
 import base from '../base';
 
 import FLStandardKnob from './fl-standard-knob';
+import FLReactiveGripDial from './fl-reactive-grip-dial';
 
 import colors from './fl-colors';
 
 export default {
   FLStandardKnob,
+  FLReactiveGripDial,
   colors,
   ...base
 };
